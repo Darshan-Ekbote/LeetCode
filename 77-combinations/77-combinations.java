df5f13b53ea01dbefe6b48e12af260a0cbc5e0eb
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-01-14 22:27:31 -0800

    Time: 44 ms (21.35%), Space: 52.4 MB (24.55%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    public List<List<Integer>> combine(int n, int k) {
+        
+        dfs( n,k, new ArrayList(),1);
+        
+        return res;
+    }
+    
+    
+    public void dfs (int n, int k , List<Integer> al,int j){
+        
+        if(al.size()==k ){
+            res.add( new ArrayList(al));
+            return;
+        }
+        
+        for( int i =j;i<=n;i++){
+            al.add(i);
+            dfs(n,k,al,i+1);
+            al.remove(al.size()-1);
+        }
+        
+    }
+}
