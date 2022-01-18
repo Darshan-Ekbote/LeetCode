6c54c8fea40faf5a334715c663c569efec999ca8
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-01-17 19:45:31 -0800

    Time: 1 ms (95.11%), Space: 39.1 MB (69.50%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        Map<Integer,Integer> counter  = new HashMap<>();
-        for( int n : nums)
-           counter.put(n,counter.getOrDefault(n,0)+1);
-        
-        for( int key : counter.keySet())
-            if(counter.get(key)==1)
-                return key;
-        
-        
-        return -1;
+  public int singleNumber(int[] nums) {
+    int a = 0;
+      /*
+      res = 7 ^ (3 ^ 3) ^ (4 ^ 4) ^ (5 ^ 5)
+          = 7 ^ 0 ^ 0 ^ 0
+          = 7 ^ 0
+          = 7
+      */
+    for (int i : nums) {
+      a ^= i;
     }
+    return a;
+  }
 }
