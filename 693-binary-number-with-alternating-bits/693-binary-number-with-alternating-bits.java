736d70a698a15b5ef8f944b315c840b1ed391235
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-02-01 13:26:55 -0800

    Time: 1 ms (40.16%), Space: 41 MB (5.25%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.java
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.java
@@ -3,19 +3,12 @@ class Solution {
         String s=  Integer.toBinaryString(n);
         int index =0;
         char temp='a';
-        
-        //101
         for( char  c : s.toCharArray()){
-            
             if( index ==0) temp =c;
-            
             else{
-                if( (temp == '0' && c=='1') || (temp=='1' && c=='0'))
-                    temp =c;
-                else
-                    return false;
+                if( (temp != c))temp =c;
+                else return false;
             }
-            
             index++;
         }
         return true;
