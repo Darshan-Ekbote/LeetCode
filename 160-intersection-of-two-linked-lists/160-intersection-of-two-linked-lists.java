b2b0baeefe104aacd057ea7af7e4c0a13e1b0f7d
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-02-17 16:15:08 -0800

    Time: 1 ms (98.90%), Space: 55.3 MB (18.66%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -8,19 +8,70 @@
  *         next = null;
  *     }
  * }
- */
+ **/
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
-        ListNode p1 = headA;
+      
+     ListNode p1 = headA;
         ListNode p2 = headB;
-       
-        while( p1!= p2){
-            p1 = p1==null? headB: p1.next;
-            p2 = p2==null? headA: p2.next;
+        int len1=0;
+        int len2=0;
+        
+        while(p1!=null){
+            p1 =  p1.next;
+            len1++;
+            
+        }
+        while( p2!=null){
+            p2 =  p2.next;
+            len2++;
+    }
+        int intersect = Math.abs(len1-len2);
+        
+         p1= headA;
+        p2= headB;
+        
+        if(len1>len2){
+            
+            while(intersect >0){
+            p1 =p1.next;
+                intersect--;
+            }
+            
+            while(p1!=p2 && p1!=null && p2!=null){
+                
+                p1=p1.next;
+                p2=p2.next;
+                
+            }
+            
+            if(p1 ==null || p2 ==null)
+             return null;
+            else
+                return p1;
+        }else{
+            while(intersect  >0) {
+                p2 =p2.next;
+                intersect--;
+            }
+            
+            while(p1!=p2 && p1!=null && p2!=null){
+                
+                p1=p1.next;
+                p2=p2.next;
+                
+            }
+            
+            if(p1 ==null || p2 ==null)
+             return null;
+            else
+                return p1;
         }
         
-        return p1;
+       
+        
+        //return null;
         
     }
 }
