f5abf978dd62390f60d933ef95e093c8780966a6
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-02-24 18:38:04 -0800

    Time: 2 ms (41.14%), Space: 54.2 MB (35.41%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+         int l =0;
+        int r = s.length-1;
+        
+       while(l<=r){
+           char t = s[l];
+           s[l]= s[r];
+           s[r]= t;
+           l++;
+           r--;
+           
+       }
+        
+    }
+}
