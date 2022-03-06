6d51a1efb9d4a198c4ad0d8d41dc4f189c9d37ee
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-03-05 18:53:39 -0800

    Time: 1 ms, Memory: 42.5 MB - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -24,12 +24,12 @@ class Solution {
         if(root1==null && root2==null) return true;
         if(root1==null || root2==null) return false;
         
-        //if(root1.val != root2.val) return false;
+        if(root1.val != root2.val) return false;
         
         boolean left = check (root1.left,root2.right);
         boolean right = check(root1.right,root2.left);
         
-        return (root1.val == root2.val )&&left && right;
+        return left && right;
         
     }
 }
