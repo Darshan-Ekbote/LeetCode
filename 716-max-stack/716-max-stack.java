aa2a3a5d0a7cc68244696161a37b28d4d30a4b66
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-03-13 18:18:46 -0700

    Time: 56 ms (12.78%), Space: 54.4 MB (43.64%) - LeetHub

---
+++ 716-max-stack/716-max-stack.java
@@ -0,0 +1,54 @@
+class MaxStack {
+
+    List<Integer> st; 
+    int maxIndex=-1;
+    
+    public MaxStack() { 
+        st =  new ArrayList<>();
+    }
+    
+    public void push(int x) {
+        st.add(x); 
+    }
+    
+    public int pop() {
+        int popped = st.remove(st.size()-1);
+        return popped;
+    }
+    
+    public int top() {
+        return st.get(st.size()-1);
+    }
+    
+    public int peekMax() {
+        int max= Integer.MIN_VALUE;
+        
+        
+        for( int i=0;i<st.size();i++){
+            
+            if(st.get(i)>=max){
+                max=st.get(i);
+                maxIndex=i;
+            }
+        }
+        
+        return max;
+    }
+    
+    public int popMax() {
+       int maxElement= peekMax();
+       int popped = st.remove(maxIndex);
+        
+       return popped;
+    }
+}
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
