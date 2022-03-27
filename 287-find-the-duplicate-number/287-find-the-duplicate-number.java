7eb48d622f623e6f5b17537c8a56ef867c16b89b
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-03-26 20:10:05 -0700

    Time: 36 ms (25.49%), Space: 93.8 MB (7.74%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set <Integer> set = new HashSet<>();
+        
+        for(int n: nums){
+            
+            if(set.contains(n))
+                return n;
+            
+            set.add(n);
+        }
+        
+        return -1;
+    }
+}
