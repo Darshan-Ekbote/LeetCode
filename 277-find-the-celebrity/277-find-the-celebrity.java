2cc0b1105028f79202e0ec9373bfef90c4463da7
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-05-29 00:29:31 -0700

    Time: 19 ms (66.32%), Space: 54.6 MB (17.28%) - LeetHub

---
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -0,0 +1,28 @@
+public class Solution extends Relation {
+    
+    private int numberOfPeople;
+    
+    public int findCelebrity(int n) {
+        numberOfPeople = n;
+        int celebrityCandidate = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(celebrityCandidate, i)) {
+                celebrityCandidate = i;
+            }
+        }
+        if (isCelebrity(celebrityCandidate)) {
+            return celebrityCandidate;
+        }
+        return -1;
+    }
+    
+    private boolean isCelebrity(int i) {
+        for (int j = 0; j < numberOfPeople; j++) {
+            if (i == j) continue; // Don't ask if they know themselves.
+            if (knows(i, j) || !knows(j, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
