e2d8cda393ccbd886262bdbff4385ca30a32adc0
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-05-31 22:50:21 -0700

    Time: 2 ms (61.25%), Space: 43.3 MB (62.27%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+      
+         List<String> res = new ArrayList();
+         backtrack(res, "", 0, 0, n);
+        return res;
+    
+}
+
+private void backtrack(List<String> res, String currentString, int open, int close, int max){
+    
+    if(currentString.length()==max*2){
+        res.add(currentString);
+        return;
+    }
+    
+    if(open<max)
+        backtrack(res, currentString + "(", open+1, close, max);
+    if(close<open)
+        backtrack(res, currentString + ")", open, close+1, max);
+    
+}
+    }
