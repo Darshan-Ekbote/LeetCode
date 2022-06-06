799a7c72b86ea5824ffec082f50e72746bfda4db
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-06-05 17:38:21 -0700

    Time: 1 ms (99.17%), Space: 55.7 MB (22.21%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -8,70 +8,50 @@
  *         next = null;
  *     }
  * }
- **/
-
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-      
-     ListNode p1 = headA;
-        ListNode p2 = headB;
-        int len1=0;
-        int len2=0;
         
-        while(p1!=null){
-            p1 =  p1.next;
-            len1++;
-            
-        }
-        while( p2!=null){
-            p2 =  p2.next;
-            len2++;
-    }
-        int intersect = Math.abs(len1-len2);
+        ListNode a = new ListNode(); a= headA;
+        ListNode b = new ListNode(); b= headB;
+        int lenA =0,lenB=0;
         
-         p1= headA;
-        p2= headB;
-        
-        if(len1>len2){
-            
-            while(intersect >0){
-            p1 =p1.next;
-                intersect--;
-            }
-            
-            while(p1!=p2 && p1!=null && p2!=null){
-                
-                p1=p1.next;
-                p2=p2.next;
-                
-            }
-            
-            if(p1 ==null || p2 ==null)
-             return null;
-            else
-                return p1;
-        }else{
-            while(intersect  >0) {
-                p2 =p2.next;
-                intersect--;
-            }
-            
-            while(p1!=p2 && p1!=null && p2!=null){
-                
-                p1=p1.next;
-                p2=p2.next;
-                
-            }
-            
-            if(p1 ==null || p2 ==null)
-             return null;
-            else
-                return p1;
+        while(a!=null) {
+            a=a.next;
+            lenA++;
         }
         
-       
+        while(b!=null) {
+            b=b.next;
+            lenB++;
+        }
         
-        //return null;
+        int diff = Math.abs(lenA-lenB);
+        b= headB;
+        a= headA;
+        ListNode rel ;
         
+        if(lenB>lenA) {
+            
+            rel=headB;
+            while(diff>0){
+              rel = rel.next; diff--;
+            }
+            while(rel!=a&& a!=null && rel!=null){
+                rel=rel.next; a=a.next;
+            }
+            return a;
+        
+        }else {
+            
+            rel=headA;
+            while(diff>0){
+                 rel = rel.next; diff--;
+            }
+            while(rel!=b && b!=null && rel!=null){
+                rel=rel.next; b=b.next;
+            }
+            return b;
+        }
     }
 }
