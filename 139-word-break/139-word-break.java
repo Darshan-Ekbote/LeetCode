09168e5ac86106f12d7604bec6c5313e7be108f7
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-07-16 20:17:03 -0700

    Time: 7 ms (71.66%), Space: 42.8 MB (71.15%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        boolean dp[] = new boolean[s.length()+1];
+        Set<String> set= new HashSet<>(wordDict);
+        dp[s.length()]=true;
+        
+        for( int i=s.length();i>=0;i--){
+            
+            for(String w : set){
+                
+                if(i+w.length()<=s.length() && s.substring(i,i+w.length()).startsWith(w))
+                    dp[i]= dp[i+w.length()];
+               
+                //found the match move to the next index in s
+                if(dp[i])
+                    break;
+            }
+        }
+        
+        return dp[0];
+}
+}
