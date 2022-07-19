bb1171c54843b6f7fcbab9208f1fa9518d234e67
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-07-19 00:30:15 -0700

    Time: 0 ms (100.00%), Space: 41.3 MB (68.73%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -4,17 +4,20 @@ class Solution {
         if(nums.length ==0 ) return 0;
         else if(nums.length ==1  ) return nums[0];
         
-        int[] dp = new int[nums.length];
+        int newRobbedHouse =0;
+        int prevHouse =0;
+        int prevPrevHouse= 0;
         
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        // 0, 0 ,1,2,3,1
         
-        //[1,2,3,1]
-        for( int i=2;i<nums.length;i++){
-            //curr + i-2  or previous one 
-            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+        for( int i=0;i<nums.length;i++){
+            
+            newRobbedHouse = Math.max(prevHouse, prevPrevHouse+nums[i] );
+            prevPrevHouse = prevHouse;
+            prevHouse = newRobbedHouse;
+            
         }
         
-        return Arrays.stream(dp).max().getAsInt();
+        return newRobbedHouse;
     }
 }
