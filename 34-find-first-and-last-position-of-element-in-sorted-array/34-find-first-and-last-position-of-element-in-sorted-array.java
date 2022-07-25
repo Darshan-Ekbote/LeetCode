f157c06295c8758ca4f3ed0ae4145bec14c15408
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-07-25 16:21:23 -0700

    Time: 0 ms (100.00%), Space: 47.5 MB (31.00%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,25 +1,51 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+       
+        int[] indices= new int[2];
         
-        int l = -1;
-        int r = -1;
+         indices[0]= search(nums, target, true);
+         indices[1]= search(nums,target,false);
         
+        return indices;
+    }
+    
+    
+    
+    public int search(int[] nums, int target, boolean isFirst){
+         int r = nums.length-1;
+         int l =0;
         
-        for( int i=0;i<nums.length;i++)
-            if(nums[i]==target){
-               l=i;
-                break;
-            }
-                
+         while( l<=r){
             
-        
-        for( int i=nums.length-1;i>=0;i--)
-            if(nums[i]==target){
-              r=i;
-                break;
-            }
+            int mid = l+(r-l)/2;
+            
+            if(nums[mid]==target){
                 
+                if(isFirst){//to get the first one
+                    //found lower index as nums[mid-1] is not the target but nums[mid] is target
+                    //also edge case is 0th index is target
+                    if(mid==l  || nums[mid-1]!=target){
+                            return mid;
+                     }
+                    //search on lower side
+                    r=mid-1;
+                    
+                }else {//to get the second one
+                    if(mid==r || nums[mid+1]!=target)
+                    return  mid;
+                    
+                    //search on right side
+                    l = mid+1;
+                }
+                
+            }
+            else if( nums[mid]< target){
+                l =mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
         
-        return new int[]{l,r};
+        return -1;
     }
 }
