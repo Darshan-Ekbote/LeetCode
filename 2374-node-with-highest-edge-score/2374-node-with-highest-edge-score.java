b71f5f9b916c08e77be37d8761925a9265179068
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-08-14 21:52:05 -0700

    Time: 392 ms (11.11%), Space: 180.6 MB (11.11%) - LeetHub

---
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        Double maxScore=0.0;
+        int maxNode= edges.length+1;
+        
+        for(int i=0;i<edges.length;i++)
+            adj.add(new ArrayList<>());
+        
+        for(int i=0;i<edges.length;i++)
+            adj.get(edges[i]).add(i);
+        
+        for(int i=0;i<adj.size();i++){
+            if(!adj.get(i).isEmpty()){
+                 Double sum = (Double) adj.get(i).stream().mapToDouble(x->x).sum();
+                 //System.out.println("sum for "+i+"  =  "+sum);
+                if(sum>maxScore){
+                    maxScore=sum;
+                     maxNode=i;
+                }else if(sum==maxScore){
+                    if(maxNode>i){
+                        maxNode=i;
+                    }
+                }
+            }
+           
+        }
+        
+        return maxNode;
+    }
+}
