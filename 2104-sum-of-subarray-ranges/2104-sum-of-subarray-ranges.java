5246bccc2f0d217257af2ed9fa720aff4bd6d18e
Darshan Ekbote <darshan.ekbote444@gmail.com>
2022-08-16 13:03:00 -0700

    Time: 41 ms (18.78%), Space: 45.7 MB (45.16%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -3,16 +3,19 @@ class Solution {
         
         int len = nums.length;
         
-        
+        long min =Integer.MAX_VALUE;
+        long max =Integer.MIN_VALUE;
         long total=0;
         
         for(int i=0;i<len;i++){
-            long min = nums[i];
-            long max = nums[i];
             
+            min = nums[i];
+            max = nums[i];
             for(int j=i+1;j<len;j++){
+                
                 min = Math.min(nums[j],min);
                 max = Math.max(nums[j],max);
+                
                 total+= max-min;
             }
         }
